fb4e66a06b4bf570021ba5d98ce5ee31c83466ac
Alex Chunet <achunet@worldbank.org>
2021-08-12 09:03:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -14,35 +14,42 @@ import requests
 import gunicorn
 import geopandas as gpd
 
-# Define popup
+# Define popup images
 encoded = base64.b64encode(requests.get('https://gamingtrend.com/wp-content/uploads/2020/05/feature-2-e1589992535441.jpg', stream=True).raw.read())
 html = '<img src="data:image/png;base64,{}">'.format
 
-iframe1 = IFrame(html(encoded.decode('UTF-8')), width=200, height=200)
-popup1 = folium.Popup(iframe1, max_width=250)
+iframe = IFrame(html(encoded.decode('UTF-8')), width=200, height=200)
+popup1 = folium.Popup(iframe, max_width=250)
+popup2 = folium.Popup(iframe, max_width=250)
+popup3 = folium.Popup(iframe, max_width=250)
 
-iframe2 = IFrame(html(encoded.decode('UTF-8')), width=200, height=200)
-popup2 = folium.Popup(iframe2, max_width=250)
-
-iframe3 = IFrame(html(encoded.decode('UTF-8')), width=200, height=200)
-popup3 = folium.Popup(iframe3, max_width=250)
-
-# Assets
+# Define stops
 cities = [dict(name="Paris - Ile de la cité", lat=48.8534, lon=2.3488, popup=popup1, color='gray'),
           dict(name="Arles", lat=43.684986, lon=4.631418, popup=popup2, color='gray'),
           dict(name="Montestruq", lat=43.432742, lon=-0.809565, popup=popup3, color='gray')]
 
 cities_pd = pd.DataFrame.from_dict(cities)
 
+# Define paths
+url = "https://raw.githubusercontent.com/alexchunet/bcac_pixart/main/assets/paths.geojson"
+paths = gpd.read_file(url)
+
 # Basemap
 m = folium.Map(location=[45.5236, 0], tiles='https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png', attr='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>', zoom_start=5)
 
-# Create markers with popup
+# Create markers with popup and links
 for i in range(0,len(cities_pd)):
     folium.Marker(location=[cities_pd.iloc[i]['lat'], cities_pd.iloc[i]['lon']],
        icon=folium.Icon(color = cities_pd.iloc[i]['color'], icon='glyphicon-record'),
        tooltip=cities_pd.iloc[i]['name'], popup=cities_pd.iloc[i]['popup']).add_to(m)
 
+for i in range(0,len(paths)):
+    folium.Choropleth(
+        paths.iloc[i]['geometry'],
+        line_weight=3,
+        line_color=paths.iloc[i]['color']).add_to(m)
+
+# Add title
 loc = '<br>Fantastic Urban Life Series'
 title_html = '''
              <h3 align="center" style="font-size:30px; color: white; background-size : 600px 300px ; background-image: url('https://c4.wallpaperflare.com/wallpaper/131/578/547/pixel-art-town-city-waneella-hd-wallpaper-preview.jpg'); margin : 0; height: 90px"><b>{}</b></h3>
@@ -51,6 +58,7 @@ title_html = '''
 m.get_root().html.add_child(folium.Element(title_html))
 html_string = m.get_root().render()
 
+# Configure and run app
 import dash_html_components as html
 app = dash.Dash(__name__)
 server = app.server
