b3a50ead43a63b18f80c11f335713ebac6a43e28
Alex Chunet <achunet@worldbank.org>
2021-10-12 16:08:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import dash
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
-app.layout = html.Div(children=[html.Iframe(id = 'map', src = "assets/index.html", width='100%', height='900')])
+app.layout = html.Div(children=[html.Iframe(id = 'map', src = "https://raw.githubusercontent.com/alexchunet/bcac_pixart/main/assets/index.html", width='100%', height='900')])
 
 if __name__ == '__main__':
     app.run_server()
