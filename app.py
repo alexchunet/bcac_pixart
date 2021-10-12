52dc1f26c048e8ae6a2a3e55c1c05363328d63c4
Alex Chunet <achunet@worldbank.org>
2021-10-12 21:43:31 +0200

    Sky version update

---
+++ app.py
@@ -0,0 +1,8 @@
+import dash
+import dash_bootstrap_components as dbc
+import dash_html_components as html
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
+app.layout = html.Div(children=[html.Iframe(id = 'map', src = "assets/index.html", width='100%', height='900')])
+
+if __name__ == '__main__':
+    app.run_server()
